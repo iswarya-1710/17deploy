844bd2b87a5b8e561e08d61939ea6bb3d880d251
iswarya-1710 <iswaryasenthilkumar17@gmail.com>
2025-11-17 19:48:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.write("Enter the metrics to predict customer satisfaction")
 
 order_price = st.number_input("order price", min_value=585,max_value=35180,step=1)
 delivery_charges  = st.number_input("deliverycharges", min_value=46.35, max_value=107.58,step=0.1)
-customer_long = st.number_input("customerlong", min_value=144.9272705,max_value=145.0198375, step=000000.1)
+customer_long = st.number_input("customerlong", min_value=144.9272705,max_value=145.0198375, step=0.0000001)
 coupon_discount = st.number_input("coupondiscount", min_value=0, max_value=20,step=1)
 order_total = st.number_input("ordertotal", min_value=639.29,max_value=33947.06, step=0.1)
 distance_to_nearest_warehouse = st.number_input("distance_to_nearest_warehouse", min_value=0.1078,max_value=2.2493, step=0.1)

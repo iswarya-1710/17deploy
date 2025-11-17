61d2b7cb7100d78537a6ea74e859cd255506043c
iswarya-1710 <iswaryasenthilkumar17@gmail.com>
2025-11-17 19:04:34 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import numpy as np
+import joblib
+model = joblib.load("model.joblib")
+st.title("Customer Satisfication")
+st.write("Enter the metrics to classify customer satisfaction")
+
+order_price = st.number_input("order price", min_value=585.0,max_value=35180.0,step=0.1)
+delivery_charges  = st.number_input("deliverycharges", min_value=46.35, max_value=107.58,step=0.1)
+customer_long = st.number_input("customerlong", min_value=144.9272705,max_value=145.0198375, step=0.1)
+coupon_discount = st.number_input("coupondiscount", min_value=0.0, max_value=20.0,step=0.1)
+order_total = st.number_input("ordertotal", min_value=639.29,max_value=33947.06, step=0.1)
+distance_to_nearest_warehouse = st.number_input("distance_to_nearest_warehouse", min_value=0.1078,max_value=2.2493, step=0.1)
+if st.button("Predict"):
+  features = np.array([[order_price, delivery_charges, customer_long,coupon_discount,order_total, distance_to_nearest_warehouse]])
+  prediction = model.predict(features)[0]
+  st.success(f"🌼 Predicted: {prediction}")
